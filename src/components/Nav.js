938bc5979cb3553d198351e003c1f5519c2ff171
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-08 06:39:33 -0800

    adding initial header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,36 @@
-import AppBar from "@material-ui/core/AppBar";
+import React from "react";
+import {
+	AppBar,
+	Toolbar,
+	ListItem,
+	IconButton,
+	ListItemText,
+	Avatar,
+	Divider,
+	List,
+	Typography,
+	Box,
+	Icon,
+} from "@material-ui/core";
+import {
+	Menu,
+	ArrowBack,
+	AssignmentInd,
+	Home,
+	Apps,
+	ContactMail,
+} from "@material-ui/icons";
 
-function Nav() {
+export default function Nav() {
 	return (
-		<AppBar>
-			<nav class='bg-dark p-2'>
-				<ul class='nav text-light justify-content-center'>
-					<li class='nav-item'>
-						<a class='nav-link' href='#aboutMe'>
-							About Me
-						</a>
-					</li>
-					<li class='nav-item'>
-						<a class='nav-link active' href='#myPortfolio'>
-							My Portfolio
-						</a>
-					</li>
-					<li class='nav-item'>
-						<a class='nav-link' href='#mySkills'>
-							My Skills
-						</a>
-					</li>
-				</ul>
-			</nav>
-		</AppBar>
+		<Box component='nav'>
+			<AppBar>
+				<Toolbar>
+					<IconButton>
+						<Menu />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 }
-
-export default Nav;
