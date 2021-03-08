6154fc1c6a8181ce8e72a918da473af24004b89b
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-07 17:05:30 -0800

    insalled material icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Button from "@material-ui/core/Button";
+
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'></header>
+			<header className='App-header'>This is a header</header>
+			<Button variant='contained' color='primary'>
+				Click Me
+			</Button>
 		</div>
 	);
 }
