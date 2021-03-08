d1ab2249c0cca829e259280ffb7f34af43b19840
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-08 07:54:44 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Nav from "./components/Nav";
 import { Container } from "@material-ui/core";
+import Header from "./components/Header";
 
 function App() {
 	return (
 		<div className='App'>
 			<Nav />
-			<Container></Container>
+			<Container>
+				<Header />
+			</Container>
 		</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+// import classes from "*.module.css";
+import {
+	Paper,
+	Card,
+	Avatar,
+	Typography,
+	Button,
+	Grid,
+} from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	paper: {
+		margin: "5px 0px",
+		padding: "10px",
+	},
+});
+
+export default function Header() {
+	const classes = useStyles();
+	return (
+		<Paper className={classes.paper}>
+			<Grid container justify='center' alignItems='center' direction='column'>
+				<Avatar />
+				<Typography variant='h5'>Abdulhakeem Dahir</Typography>
+				<Typography variant='h6'>Full Stack Web Developer</Typography>
+			</Grid>
+		</Paper>
+	);
+}
