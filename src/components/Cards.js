de19a2de09738e8f50fea89b51e0c4390e9c90da
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-08 13:37:07 -0800

    styling cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,7 +14,7 @@ import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Grid } from "@material-ui/core";
 // import Projects from "./Projects";
 
 const useStyles = makeStyles(theme => ({
@@ -39,7 +39,7 @@ const useStyles = makeStyles(theme => ({
 		backgroundColor: red[500],
 	},
 	paper: {
-		margin: "5px 0px",
+		margin: "5px 5px",
 		padding: "10px",
 		background: "rgba( 255, 255, 255, 0.25 )",
 		boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
@@ -60,63 +60,66 @@ export default function Cards(props) {
 	};
 	console.log(props);
 	return (
-		<Card className={classes.root}>
-			<Typography variant='h5'>{props.title}</Typography>
-			<CardMedia className={classes.media} image={props.media} />
-			<CardContent>
-				<Typography variant='body2' color='textSecondary' component='p'>
-					{props.summary}
-				</Typography>
-			</CardContent>
-			<CardActions disableSpacing>
-				<IconButton aria-label='add to favorites'>
-					<FavoriteIcon />
-				</IconButton>
-				<IconButton aria-label='share'>
-					<ShareIcon />
-				</IconButton>
-				<IconButton
-					className={clsx(classes.expand, {
-						[classes.expandOpen]: expanded,
-					})}
-					onClick={handleExpandClick}
-					aria-expanded={expanded}
-					aria-label='show more'
-				>
-					<ExpandMoreIcon />
-				</IconButton>
-			</CardActions>
-			<Collapse in={expanded} timeout='auto' unmountOnExit>
+		<Grid item xs={12} sm={4}>
+			<Card className={classes.paper}>
+				<Typography variant='h5'>{props.title}</Typography>
+				<CardMedia className={classes.media} image={props.media} />
 				<CardContent>
-					<Typography paragraph>Method:</Typography>
-					<Typography paragraph>
-						Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-						set aside for 10 minutes.
-					</Typography>
-					<Typography paragraph>
-						Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-						over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-						stirring occasionally until lightly browned, 6 to 8 minutes.
-						Transfer shrimp to a large plate and set aside, leaving chicken and
-						chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-						onion, salt and pepper, and cook, stirring often until thickened and
-						fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-						cups chicken broth; bring to a boil.
-					</Typography>
-					<Typography paragraph>
-						Add rice and stir very gently to distribute. Top with artichokes and
-						peppers, and cook without stirring, until most of the liquid is
-						absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-						shrimp and mussels, tucking them down into the rice, and cook again
-						without stirring, until mussels have opened and rice is just tender,
-						5 to 7 minutes more. (Discard any mussels that don’t open.)
-					</Typography>
-					<Typography>
-						Set aside off of the heat to let rest for 10 minutes, and then
-						serve.
+					<Typography variant='body2' color='textSecondary' component='p'>
+						{props.summary}
 					</Typography>
 				</CardContent>
-			</Collapse>
-		</Card>
+				<CardActions disableSpacing>
+					<IconButton aria-label='add to favorites'>
+						<FavoriteIcon />
+					</IconButton>
+					<IconButton aria-label='share'>
+						<ShareIcon />
+					</IconButton>
+					<IconButton
+						className={clsx(classes.expand, {
+							[classes.expandOpen]: expanded,
+						})}
+						onClick={handleExpandClick}
+						aria-expanded={expanded}
+						aria-label='show more'
+					>
+						<ExpandMoreIcon />
+					</IconButton>
+				</CardActions>
+				<Collapse in={expanded} timeout='auto' unmountOnExit>
+					<CardContent>
+						<Typography paragraph>Method:</Typography>
+						<Typography paragraph>
+							Heat 1/2 cup of the broth in a pot until simmering, add saffron
+							and set aside for 10 minutes.
+						</Typography>
+						<Typography paragraph>
+							Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+							over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+							stirring occasionally until lightly browned, 6 to 8 minutes.
+							Transfer shrimp to a large plate and set aside, leaving chicken
+							and chorizo in the pan. Add pimentón, bay leaves, garlic,
+							tomatoes, onion, salt and pepper, and cook, stirring often until
+							thickened and fragrant, about 10 minutes. Add saffron broth and
+							remaining 4 1/2 cups chicken broth; bring to a boil.
+						</Typography>
+						<Typography paragraph>
+							Add rice and stir very gently to distribute. Top with artichokes
+							and peppers, and cook without stirring, until most of the liquid
+							is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+							reserved shrimp and mussels, tucking them down into the rice, and
+							cook again without stirring, until mussels have opened and rice is
+							just tender, 5 to 7 minutes more. (Discard any mussels that don’t
+							open.)
+						</Typography>
+						<Typography>
+							Set aside off of the heat to let rest for 10 minutes, and then
+							serve.
+						</Typography>
+					</CardContent>
+				</Collapse>
+			</Card>
+		</Grid>
 	);
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,50 @@
 import React, { useState } from "react";
 import Cards from "./Cards";
+import { Container, Grid } from "@material-ui/core";
+import Project1 from "../images/project-1.png";
+import Project2 from "../images/project-2.png";
+import Homework5 from "../images/homework5.png";
+import { CssBaseline } from "@material-ui/core";
 
 export default function Projects() {
 	const [projectState, setProjectState] = useState([
 		{
 			title: "Project 2",
-			media: "",
-			summary: "Lorem Ipsum",
+			media: Project2,
+			summary:
+				"One World Academy is a learning management system where users can create courses and browse courses created by other users.",
 		},
 		{
 			title: "Project 3",
-			media: "",
-			summary: "Lorem Ipsum",
+			media: Project1,
+			summary:
+				"This was our first class project. We decided to create a dynamic State Covid tracker as a resource for Travelers.",
+		},
+		{
+			title: "Project 4",
+			media: Homework5,
+			summary:
+				"The Day Planner Application required us to navigate the DOM and to incorporate Local Storage as well as use jQuery.",
 		},
 	]);
 	return (
-		<div>
-			{projectState.map(card => (
-				<Cards title={card.title} media={card.media} summary={card.summary} />
-			))}
-		</div>
+		<>
+			<CssBaseline>
+				<Grid
+					container
+					direction='row'
+					justify='space-evenly'
+					alignItems='center'
+				>
+					{projectState.map(card => (
+						<Cards
+							title={card.title}
+							media={card.media}
+							summary={card.summary}
+						/>
+					))}
+				</Grid>
+			</CssBaseline>
+		</>
 	);
 }
