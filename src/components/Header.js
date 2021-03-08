b94174293e3745bdad519919c8810bddd3843c32
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-08 08:34:38 -0800

    added buttons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,21 @@ import {
 	Avatar,
 	Typography,
 	Button,
+	ButtonGroup,
 	Grid,
 } from "@material-ui/core";
-
 import { makeStyles } from "@material-ui/core/styles";
+import Github from "@material-ui/icons/GitHub";
+import LinkedIn from "@material-ui/icons/LinkedIn";
+import Pages from "@material-ui/icons/Pages";
 
 const useStyles = makeStyles({
 	paper: {
 		margin: "5px 0px",
 		padding: "10px",
+	},
+	button: {
+		margin: "5px 5px",
 	},
 });
 
@@ -24,7 +30,41 @@ export default function Header() {
 			<Grid container justify='center' alignItems='center' direction='column'>
 				<Avatar />
 				<Typography variant='h5'>Abdulhakeem Dahir</Typography>
-				<Typography variant='subtitle1'>Full Stack Web Developer</Typography>
+				<Typography variant='subtitle1' style={{ fontStyle: "italic" }}>
+					Full Stack Web Developer
+				</Typography>
+				<Typography variant='subtitle1'>
+					<span style={{ fontWeight: "bold" }}>E-mail:</span>
+					somlutions@gmail.com
+				</Typography>
+				<Grid direction='row'>
+					<ButtonGroup>
+						<Button
+							startIcon={<Github />}
+							href='https://github.com/abdulhakeemdahir'
+							variant='contained'
+							color='primary'
+						>
+							GitHub
+						</Button>
+						<Button
+							startIcon={<LinkedIn />}
+							href='https://github.com/abdulhakeemdahir'
+							variant='contained'
+							color='secondary'
+						>
+							LinkedIn
+						</Button>
+						<Button
+							startIcon={<Pages />}
+							href='https://github.com/abdulhakeemdahir'
+							variant='contained'
+							color='primary'
+						>
+							Resume
+						</Button>
+					</ButtonGroup>
+				</Grid>
 			</Grid>
 		</Paper>
 	);
